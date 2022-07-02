fb1180a1f5e3fca43e02ab3af5e86c7fd8345944
Mofachhal Hossain <mofachhalhossain101@gmail.com>
2022-07-02 19:49:40 +0600

    ..

---
+++ main.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector("nav");
+const mobileNav = document.querySelector("nav.mobile-nav");
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 60) {
+        nav.classList.add("scrolled");
+        mobileNav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+        nav.classList.remove("scrolled");
+    }
+})
